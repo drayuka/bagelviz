2b2b9c7095ff175309d4a9eda7abc993453fe305
Clifton Tyler <ct12067@gmail.com>
2023-07-17 18:44:31 -0500

    adding https

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   resolve: {
     alias: {
       'node-fetch': 'isomorphic-fetch',
     }
   },
+  server: {
+    https: true,
+    port: 443,
+  }
 })
